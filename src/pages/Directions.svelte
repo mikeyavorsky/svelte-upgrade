32b4272f03a09238e9e59679888ccb2f8b9f4731
Mike Yavorsky <mikeyavorsky@fastmail.com>
2021-08-08 12:33:58 -0400

    add embedded map

--- src/pages/Directions.svelte
+++ src/pages/Directions.svelte
@@ -6,7 +6,8 @@
 </script>
 
 <main>
-  Directions
+  <header>Directions</header>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2962.1198624540157!2d-70.91650603424827!3d42.062050111860884!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e49a2cd21e5207%3A0x4ce2d376c7bb228b!2sThe%20Villa%20at%20Ridder%20Country%20Club!5e0!3m2!1sen!2sus!4v1628440410455!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 </main>
 
 <style>
